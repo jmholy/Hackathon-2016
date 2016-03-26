d37de832b274f047e536712d784bba31749b4143
HunterWilkes <hunter.wilkes@wsu.edu>
2016-03-26 13:39:02 -0700

    Also Nothing, just trying to get all the stuff we already have

--- HMM P3/WindowsFormsApplication1/Form1.Designer.cs
+++ HMM P3/WindowsFormsApplication1/Form1.Designer.cs
@@ -32,16 +32,19 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 453);
+            this.ClientSize = new System.Drawing.Size(325, 294);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "HMM P3";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
     }
 }
 
--- HMM P3/WindowsFormsApplication1/Form1.cs
+++ HMM P3/WindowsFormsApplication1/Form1.cs
@@ -17,5 +17,10 @@ namespace WindowsFormsApplication1
             InitializeComponent();
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
