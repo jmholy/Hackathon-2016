61912578dd722f816e0b127adea91ddcdb414683
jmholy <jemarho@msn.com>
2016-03-26 19:54:54 -0700

    Working on progress bar

--- HMM P3/Form1.Designer.cs
+++ HMM P3/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.play = new System.Windows.Forms.Button();
             this.pause = new System.Windows.Forms.Button();
@@ -36,13 +37,14 @@
             this.volumedown = new System.Windows.Forms.Button();
             this.volumeup = new System.Windows.Forms.Button();
             this.progressBar = new System.Windows.Forms.ProgressBar();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.filesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.addSongsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timeBox1 = new System.Windows.Forms.TextBox();
+            this.timeBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,10 +53,10 @@
             this.play.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.play.AutoSize = true;
             this.play.Image = ((System.Drawing.Image)(resources.GetObject("play.Image")));
-            this.play.Location = new System.Drawing.Point(99, 325);
+            this.play.Location = new System.Drawing.Point(148, 500);
             this.play.Margin = new System.Windows.Forms.Padding(0);
             this.play.Name = "play";
-            this.play.Size = new System.Drawing.Size(89, 87);
+            this.play.Size = new System.Drawing.Size(134, 134);
             this.play.TabIndex = 1;
             this.play.UseVisualStyleBackColor = true;
             this.play.Click += new System.EventHandler(this.play_Click);
@@ -64,10 +66,10 @@
             this.pause.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.pause.AutoSize = true;
             this.pause.Image = ((System.Drawing.Image)(resources.GetObject("pause.Image")));
-            this.pause.Location = new System.Drawing.Point(191, 325);
+            this.pause.Location = new System.Drawing.Point(286, 500);
             this.pause.Margin = new System.Windows.Forms.Padding(0);
             this.pause.Name = "pause";
-            this.pause.Size = new System.Drawing.Size(89, 87);
+            this.pause.Size = new System.Drawing.Size(134, 134);
             this.pause.TabIndex = 2;
             this.pause.UseVisualStyleBackColor = true;
             this.pause.Click += new System.EventHandler(this.pause_Click);
@@ -77,10 +79,10 @@
             this.skipforward.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.skipforward.AutoSize = true;
             this.skipforward.Image = ((System.Drawing.Image)(resources.GetObject("skipforward.Image")));
-            this.skipforward.Location = new System.Drawing.Point(283, 325);
+            this.skipforward.Location = new System.Drawing.Point(424, 500);
             this.skipforward.Margin = new System.Windows.Forms.Padding(0);
             this.skipforward.Name = "skipforward";
-            this.skipforward.Size = new System.Drawing.Size(89, 87);
+            this.skipforward.Size = new System.Drawing.Size(134, 134);
             this.skipforward.TabIndex = 3;
             this.skipforward.UseVisualStyleBackColor = true;
             this.skipforward.Click += new System.EventHandler(this.skipforward_Click);
@@ -90,10 +92,10 @@
             this.skipback.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.skipback.AutoSize = true;
             this.skipback.Image = ((System.Drawing.Image)(resources.GetObject("skipback.Image")));
-            this.skipback.Location = new System.Drawing.Point(7, 325);
+            this.skipback.Location = new System.Drawing.Point(10, 500);
             this.skipback.Margin = new System.Windows.Forms.Padding(0);
             this.skipback.Name = "skipback";
-            this.skipback.Size = new System.Drawing.Size(89, 87);
+            this.skipback.Size = new System.Drawing.Size(134, 134);
             this.skipback.TabIndex = 6;
             this.skipback.UseVisualStyleBackColor = true;
             this.skipback.Click += new System.EventHandler(this.skipback_Click);
@@ -103,10 +105,10 @@
             this.volumedown.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.volumedown.AutoSize = true;
             this.volumedown.Image = ((System.Drawing.Image)(resources.GetObject("volumedown.Image")));
-            this.volumedown.Location = new System.Drawing.Point(375, 325);
+            this.volumedown.Location = new System.Drawing.Point(562, 500);
             this.volumedown.Margin = new System.Windows.Forms.Padding(0);
             this.volumedown.Name = "volumedown";
-            this.volumedown.Size = new System.Drawing.Size(89, 87);
+            this.volumedown.Size = new System.Drawing.Size(134, 134);
             this.volumedown.TabIndex = 8;
             this.volumedown.UseVisualStyleBackColor = true;
             this.volumedown.Click += new System.EventHandler(this.volumedown_Click);
@@ -116,10 +118,10 @@
             this.volumeup.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.volumeup.AutoSize = true;
             this.volumeup.Image = ((System.Drawing.Image)(resources.GetObject("volumeup.Image")));
-            this.volumeup.Location = new System.Drawing.Point(467, 325);
+            this.volumeup.Location = new System.Drawing.Point(700, 500);
             this.volumeup.Margin = new System.Windows.Forms.Padding(0);
             this.volumeup.Name = "volumeup";
-            this.volumeup.Size = new System.Drawing.Size(89, 87);
+            this.volumeup.Size = new System.Drawing.Size(134, 134);
             this.volumeup.TabIndex = 9;
             this.volumeup.UseVisualStyleBackColor = true;
             this.volumeup.Click += new System.EventHandler(this.volumeup_Click);
@@ -129,36 +131,21 @@
             this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.progressBar.ForeColor = System.Drawing.SystemColors.InfoText;
-            this.progressBar.Location = new System.Drawing.Point(0, 304);
-            this.progressBar.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar.Location = new System.Drawing.Point(0, 468);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(564, 16);
+            this.progressBar.Size = new System.Drawing.Size(846, 25);
             this.progressBar.TabIndex = 10;
             this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.textBox1.Location = new System.Drawing.Point(0, 278);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 11;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(464, 278);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 12;
-            // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.filesToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(565, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(848, 35);
             this.menuStrip1.TabIndex = 13;
             // 
             // filesToolStripMenuItem
@@ -166,13 +153,13 @@
             this.filesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addSongsToolStripMenuItem});
             this.filesToolStripMenuItem.Name = "filesToolStripMenuItem";
-            this.filesToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.filesToolStripMenuItem.Size = new System.Drawing.Size(58, 29);
             this.filesToolStripMenuItem.Text = "Files";
             // 
             // addSongsToolStripMenuItem
             // 
             this.addSongsToolStripMenuItem.Name = "addSongsToolStripMenuItem";
-            this.addSongsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addSongsToolStripMenuItem.Size = new System.Drawing.Size(186, 30);
             this.addSongsToolStripMenuItem.Text = "Add Songs";
             this.addSongsToolStripMenuItem.Click += new System.EventHandler(this.addSongsToolStripMenuItem_Click);
             // 
@@ -186,13 +173,35 @@
             this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
             this.loadToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
             // 
+            // timeBox1
+            // 
+            this.timeBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.timeBox1.Location = new System.Drawing.Point(0, 428);
+            this.timeBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.timeBox1.Name = "timeBox1";
+            this.timeBox1.Size = new System.Drawing.Size(148, 26);
+            this.timeBox1.TabIndex = 11;
+            // 
+            // timeBox2
+            // 
+            this.timeBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.timeBox2.Location = new System.Drawing.Point(696, 428);
+            this.timeBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.timeBox2.Name = "timeBox2";
+            this.timeBox2.Size = new System.Drawing.Size(148, 26);
+            this.timeBox2.TabIndex = 12;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 422);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(848, 649);
+            this.Controls.Add(this.timeBox2);
+            this.Controls.Add(this.timeBox1);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.volumeup);
             this.Controls.Add(this.volumedown);
@@ -202,7 +211,6 @@
             this.Controls.Add(this.play);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "HMM P3";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -222,13 +230,14 @@
         private System.Windows.Forms.Button volumedown;
         private System.Windows.Forms.Button volumeup;
         private System.Windows.Forms.ProgressBar progressBar;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem filesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem addSongsToolStripMenuItem;
+        private System.Windows.Forms.TextBox timeBox1;
+        private System.Windows.Forms.TextBox timeBox2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
